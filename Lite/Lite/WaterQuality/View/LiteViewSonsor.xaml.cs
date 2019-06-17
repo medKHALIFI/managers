36fa03d769869abea62b6c8f2615dd4ebc802aed
KHALIFI Mohamed <med.khalifi95@gmail.com>
2019-06-17 11:04:59 +0100

    view sensor

--- Lite/Lite/WaterQuality/View/LiteViewSonsor.xaml.cs
+++ Lite/Lite/WaterQuality/View/LiteViewSonsor.xaml.cs
@@ -17,6 +17,38 @@ namespace Lite
         public LiteViewSonsor()
         {
             InitializeComponent();
+            // add listener to list box sensor
+            listBox_sensor.SelectionChanged += OnSelectionChanged;
+        }
+
+        // test listener 
+        //listbox changed
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // MessageBox.Show(" test");
+
+            //Do something with the selected item
+
+            // todo add here laboratory from database
+            /* label_stat_view.Content = "ready";
+             label_stat_view.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 250, 0));
+             label_laboratory_view.Content = "laboratory " + listBox_mission.SelectedIndex;
+             label_labassistant_view.Content = "labassistant " + listBox_mission.SelectedIndex;
+             */
+            // Instantiate random number generator using system-supplied value as seed.
+            Random rand = new Random();
+            // Generate and display 5 random byte (integer) values.
+            byte[] bytes = new byte[5];
+            rand.NextBytes(bytes);
+
+            label_id_view.Content = rand.Next();
+            label_stat_view.Content = "ready";
+            label_stat_view.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 250, 0));
+            label_type_view.Content = "temperature";
+            label_name_view.Content = "sensor " + listBox_sensor.SelectedIndex;
+
+
+
         }
     }
 }
